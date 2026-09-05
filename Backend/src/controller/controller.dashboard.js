d2291cb0600ecf31b8a5d76741735cdbea5dd6ff
spystar20 <hert5197@gmail.com>
2026-09-05 20:13:58 +0530

    .

--- Backend/src/controller/controller.dashboard.js
+++ Backend/src/controller/controller.dashboard.js
@@ -4,12 +4,20 @@ import Course from '../models/Teacher/Course.js'
 import enrollmentModel from '../models/Teacher/Enrollment.js'
 export const studentDashboardData = asyncHandler(async(req,res)=>{
     const user = req.user.UserID
-    const existingEnrollment = await enrollmentModel.find({userId:user})
+    const existingEnrollment = await enrollmentModel.find({userId:user}).populate("userId","firstName avatar")
     if(existingEnrollment.length===0){
         return res.status(404).json({message:"enrolled user not found"})
     }
+
    const enrolledCourses = existingEnrollment
    const completedCourses = existingEnrollment.filter(enrolledCourse=>enrolledCourse.completed)
+   const continueCourses = await Promise.all(enrolledCourses.filter(enrolledCourse=>enrolledCourse.status==="in-progress").map(async enrolledCourse=>{
+const course= await Course.findById(enrolledCourse.courseId).populate("instructor","firstName avatar").lean()
+if(!course){
+Promise.reject("course not found")
+}
+return {...course}
+   }))
    const learningHours = Math.floor( enrolledCourses.map(enrolledCourse=>{
      const result = enrolledCourse.learningActivity.reduce((acc,curr)=>acc+curr.watchedTime,0)
    return result
@@ -17,9 +25,10 @@ export const studentDashboardData = asyncHandler(async(req,res)=>{
    ).reduce((acc,curr)=>acc+curr,0)
 ) 
 const certificates = await Promise.all( enrolledCourses.map(async enrolledCourse=>{
-const existingCertificate = await certficateModel.find({enrollmentId:enrolledCourse._id})
-return existingCertificate
-}))
-console.log(certificates)
-    return res.status(200).json({enrolledCourses,completedCourses,learningHours,certificates})
+const existingCertificate = await certficateModel.findOne({enrollmentId:enrolledCourse._id})
+return existingCertificate}))
+
+const certificateCount = certificates.filter(certificate=>certificate!==null).length
+
+    return res.status(200).json({enrolledCourses,completedCourses,learningHours,certificateCount,continueCourses})
 })
